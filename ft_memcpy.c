030ace9e5ec6cd1972e85cf4fc130ca7031b2c7b
Elliot D'INGUIMBERT <edinguim@e1r7p7.42.fr>
2017-11-10 17:24:42 +0100

    push

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edinguim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/10 16:30:17 by edinguim          #+#    #+#             */
-/*   Updated: 2017/11/10 17:22:07 by edinguim         ###   ########.fr       */
+/*   Created: 2017/11/10 17:12:51 by edinguim          #+#    #+#             */
+/*   Updated: 2017/11/10 17:16:01 by edinguim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+void	*ft_memcpy(void *dest, const void *src)
 {
 	unsigned char		*dptr;
 	const unsigned char	*sptr;
 
 	dptr = dest;
 	sptr = src;
-	while (n-- && *sptr != (unsigned char)c)
+	while (*sptr)
 		*dptr++ = *sptr++;
 	return (dest);
 }
--- ft_memncpy.c
+++ ft_memncpy.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*   ft_memncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edinguim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/10 16:30:17 by edinguim          #+#    #+#             */
-/*   Updated: 2017/11/10 17:22:07 by edinguim         ###   ########.fr       */
+/*   Created: 2017/11/10 17:17:55 by edinguim          #+#    #+#             */
+/*   Updated: 2017/11/10 17:21:47 by edinguim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+void	*ft_memncpy(void *dest, const void *src, size_t n)
 {
 	unsigned char		*dptr;
 	const unsigned char	*sptr;
 
 	dptr = dest;
 	sptr = src;
-	while (n-- && *sptr != (unsigned char)c)
+	while(n-- && *sptr)
 		*dptr++ = *sptr++;
 	return (dest);
 }
