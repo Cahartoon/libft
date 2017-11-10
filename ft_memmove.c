9e99be20921d595a1ae5c5172fb442c90e061dc4
Elliot D'INGUIMBERT <edinguim@e1r7p7.42.fr>
2017-11-10 17:58:56 +0100

    push

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edinguim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/10 17:30:38 by edinguim          #+#    #+#             */
-/*   Updated: 2017/11/10 17:31:14 by edinguim         ###   ########.fr       */
+/*   Created: 2017/11/10 17:28:08 by edinguim          #+#    #+#             */
+/*   Updated: 2017/11/10 17:46:49 by edinguim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <string.h>
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char		*dptr;
+	unsigned char		*tmp;
 	const unsigned char	*sptr;
 
 	dptr = dest;
 	sptr = src;
-	while(n-- && *sptr)
-		*dptr++ = *sptr++;
 	return (dest);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: edinguim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 18:36:42 by edinguim          #+#    #+#             */
-/*   Updated: 2017/11/10 17:22:30 by edinguim         ###   ########.fr       */
+/*   Updated: 2017/11/10 17:46:45 by edinguim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,9 +49,9 @@ char	*ft_strtrim(const char *s);
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 void	*ft_memset(void *s, int c, size_t l);
-void	*ft_memcpy(void *dest, const void *src);
-void	*ft_memncpy(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 int		ft_isupper(int c);
 int		ft_islower(int c);
